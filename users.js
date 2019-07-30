e51afc347fde0a6652fc86993a6a896b1d34d651
Kim Taehyung <33085835+Zerapium@users.noreply.github.com>
2019-07-30 17:36:53 +0530

    Fix hasBotRank() bug

--- users.js
+++ users.js
@@ -4,7 +4,7 @@
  *
  * This file tracks information about users.
  *
- * @license MIT license
+ * @lcense MIT license
  */
 
 'use strict';
@@ -19,12 +19,17 @@ class User {
 	constructor(name, id) {
 		this.name = Tools.toName(name);
 		this.id = id;
+    this.status = null;
+    this.avatar = null;
 		/**@type {Map<Room, string>} */
 		this.rooms = new Map();
 		/**@type {Map<Room, {messages: Array<{time: number, message: string}>, points: number, lastAction: number}>} */
 		this.roomData = new Map();
 		/**@type {?Game} */
 		this.game = null;
+ //   this.level = true;
+ //   if(Config.bannedLevel.includes(this.id)) this.level = false;
+    this.globalrank = null;
 	}
 
 	/**
@@ -33,6 +38,13 @@ class User {
 	 * @return {boolean}
 	 */
 	hasRank(room, targetRank) {
+    if(room.id === this.id){
+      let userid = Tools.toId(this);
+      userid = userid.split('');
+      if(userid[0] === '+') return true;
+      return false;
+    }
+    else{
 		if (!Config.groups) return false;
 		let rank;
 		if (typeof room === 'string') {
@@ -43,7 +55,17 @@ class User {
 		if (!rank) return false;
 		return Config.groups[rank] >= Config.groups[targetRank];
 	}
+  }
 
+    hasBotRank(room, targetRank){
+ if(this.isDeveloper()) return true;
+    if(!Storage.databases[room.id].auth) return false;
+   let rank =  Storage.databases[room.id].auth[this.id]
+    if(Config.groups[rank] >= Config.groups[targetRank]) return true;
+    return false;
+  }
+  
+  
 	/**
 	 * @return {boolean}
 	 */
@@ -105,6 +127,32 @@ class Users {
 			}
 		}
 	}
+  
+   remove(user) {
+        if (user !== this.self)
+            delete this.users[user.id];
+    }
+  
+    rename(name, oldId) {
+        if (!(oldId in this.users))
+            return this.add(name);
+        const user = this.users[oldId];
+        this.remove(user);
+        const id = Tools.toId(name);
+        if (id in this.users)
+            return this.users[id];
+        user.name = name;
+        user.id = id;
+        this.users[id] = user;
+        user.rooms.forEach((value, room) => {
+            if (room.game)
+                room.game.renamePlayer(user, oldId);
+            if (room.tournament)
+                room.tournament.renamePlayer(user, oldId);
+        });
+        return user;
+    }
+  
 
 	destroyUsers() {
 		for (let i in this.users) {
